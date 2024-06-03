5f0ec1a2dae6cb5b7873f30eff828fda40bd10b2
Moarz Naing Lin Khant <nainglinkhant2005@gmail.com>
2024-06-04 03:02:40 +0800

    Turns out it's as simple as this

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -4,20 +4,8 @@ def isPalindrome( x):
         :rtype: bool
         """
         inString = str(x)
-        
-        if len(inString) % 2 != 0: 
-           num = (len(inString) - 1)//2
-        else:
-           num = len(inString)//2
-        
-        left=  inString[:num]
-        right=  inString[-num:]
-        print(left)
-        print(right)
-        
-        if left == right[::-1]: 
-            return True  
-        else: 
-           return False 
-
+        # Compare the string with its reverse
+        return inString == inString[::-1]
+    
+    
 isPalindrome(121)
